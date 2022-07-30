83c55ba405bb6087f2ec05e45b8c0d73f6624018
Evgenia Sivtsova <evgenia.sivtsova@gmail.com>
2022-07-30 16:18:21 +0100

    githubRequest: updated

--- phase-two/05_fetch_data_from_APIs/githubRequest.js
+++ phase-two/05_fetch_data_from_APIs/githubRequest.js
@@ -2,17 +2,8 @@ const got = require('got');
 
 const url = 'https://api.github.com/repos/sinatra/sinatra';
 
-// VERSION 1:
-
 got(url)
 .then((response) => {
-    console.log(JSON.parse(response.body));
+    const githubObject = JSON.parse(response.body);
+    console.log(githubObject);
 });
-
-// VERSION 2:
-
-// const handleReceivedResponse = (response) => {
-//   console.log(JSON.parse(response.body));
-// }
-
-// got(url).then(handleReceivedResponse);
